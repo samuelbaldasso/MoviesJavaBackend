fe8a0159406d3db3e6fc6bd4897a7974f2664bf1
Samuel Baldasso <samuelbaldasso93@gmail.com>
2024-01-01 07:48:09 -0300

    first commit

---
+++ src/test/java/com/sbaldass/movies_backend/MoviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbaldass.movies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

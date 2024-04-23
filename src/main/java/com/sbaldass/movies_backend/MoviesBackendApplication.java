ed7ebd5702860c2742e870a0756bca7daf8fe7a7
samuelbaldasso <samuelbaldasso93@gmail.com>
2024-04-23 15:13:09 -0300

    bugfix: ant path matcher

--- src/main/java/com/sbaldass/movies_backend/MoviesBackendApplication.java
+++ src/main/java/com/sbaldass/movies_backend/MoviesBackendApplication.java
@@ -6,8 +6,6 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@EnableOpenApi
 public class MoviesBackendApplication {
 
     public static void main(String[] args) {
